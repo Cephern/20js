c9a471ce3d3b67bd115417c84fb2d6b2c0a0ee10
JagMax <gewoonmaks@gmail.com>
2020-04-09 20:51:57 +0300

    Add Retry btn, fixed timer add up without game being started

--- typing game/main.js
+++ typing game/main.js
@@ -7,6 +7,7 @@ const settingsBtn = document.getElementById("settings-btn");
 const settings = document.getElementById("settings");
 const settingsForm = document.getElementById("settings-form");
 const difficultySelect = document.getElementById("difficulty");
+const startBtn = document.getElementById("start-btn");
 
 // List of words
 
@@ -32,8 +33,10 @@ const words = [
 ];
 
 //  Init word
-
 let randomWord;
+
+// No dublicate words in a row
+let previousWord;
 
 // Init score
 let score = 0;
@@ -41,8 +44,15 @@ let score = 0;
 // Init time
 let time = 10;
 
-// Start counting down
-const timeInterval = setInterval(updateTime, 1000);
+// Game ended tracker
+
+let gameEnded = false;
+
+// No time abuse
+let gameStarted = false;
+
+//  Init countdown
+let timeInterval;
 
 let difficulty = localStorage.getItem("difficulty") || "medium";
 
@@ -50,7 +60,7 @@ let difficulty = localStorage.getItem("difficulty") || "medium";
 difficultySelect.value = difficulty;
 
 // Focus on input on start
-text.focus();
+startBtn.focus();
 
 function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
@@ -58,6 +68,14 @@ function getRandomWord() {
 
 function addWordToDOM() {
   randomWord = getRandomWord();
+  if (randomWord == previousWord) {
+    index = words.indexOf(randomWord);
+    index < words.length - 1
+      ? (randomWord = words[index + 1])
+      : (randomWord = words[index - 1]);
+  }
+
+  previousWord = randomWord;
   word.innerHTML = randomWord;
 }
 
@@ -80,13 +98,12 @@ function gameOver() {
   endgameEl.innerHTML = `
         <h1>Time ran out</h1>
         <p>Your final score is ${score}</p>
-        <button onclick="location.reload()">Reload</button>
     `;
 
   endgameEl.style.display = "flex";
+  gameEnded = true;
+  startBtn.innerText = "Retry";
 }
-
-// Settings
 
 // Inits
 
@@ -100,16 +117,15 @@ text.addEventListener("input", (e) => {
     addWordToDOM();
 
     updateScore();
-
-    if (difficulty === "hard") {
-      time += 2;
-    } else if (difficulty === "medium") {
-      time += 3;
-    } else {
-      time += 5;
+    if (gameStarted) {
+      if (difficulty === "hard") {
+        time += 2;
+      } else if (difficulty === "medium") {
+        time += 3;
+      } else {
+        time += 5;
+      }
     }
-
-    updateTime();
   }
 });
 
@@ -118,4 +134,15 @@ settingsBtn.addEventListener("click", () => settings.classList.toggle("hide"));
 settingsForm.addEventListener("change", (e) => {
   difficulty = e.target.value;
   localStorage.setItem("difficulty", difficulty);
+});
+
+startBtn.addEventListener("click", () => {
+  gameStarted = true;
+  timeInterval = setInterval(updateTime, 1000);
+  text.focus();
+  updateTime();
+
+  if (gameEnded) {
+    location.reload();
+  }
 });
